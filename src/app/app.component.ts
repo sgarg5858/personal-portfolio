f499962e4ed4398fc23681abacc1bd2d6b164bf5
Sanjay Garg <sgarg5858@gmail.com>
2023-10-17 13:31:21 +0530

    Created Custom theme in just 4 simple steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import {MatButtonModule} from '@angular/material/button'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone:true
+  standalone:true,
+  imports:[MatButtonModule]
 })
 export class AppComponent {
   title = 'sanjay-portfolio';
